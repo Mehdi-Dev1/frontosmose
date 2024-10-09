7b5c13ecb4513bb54d8caa3a039442f130984502
mohamed manseur <mohamedmanseur@MacBook-Pro-de-mohamed.local>
2024-10-09 18:38:39 +0200

    routing is correctly perfect

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,12 +1,13 @@
 // src/App.tsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-const App: React.FC = () => {
+const Admin: React.FC = () => {
+
   return (   
-    <>dffddfv
+    <>
     <Outlet/>
     </>
   );
 };
 
-export default App;
+export default Admin;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import {  Route } from 'react-router-dom';
+import {Link } from 'react-router-dom';
 import Sidebar from '../components/sidebare/Sidebar'; // Assure-toi d'importer le bon chemin
-import Artistes from "./ArtistesDashboard/artist"
-// import CreateArtist from "./pages/ArtistesDashboard/CreateArtist"
-// import StateExposition from "./pages/ExpositionDashboard/StateExposition"
-// import StateArtist from "./pages/ArtistesDashboard/StateArtist"
 
-// import CreateOeuvre from "./pages/OeuvresDashboard/CreateOeuvre"
-
-// import StateOeuvre from "./pages/OeuvresDashboard/StateOeuvre"
-// import Exposition from "./pages/ExpositionDashboard/Exposition"
-// import CreateExposition from "./pages/ExpositionDashboard/CreateExposition"
 
 const Dashboard: React.FC = () => {
   return (
     <div className="flex">
-      dffdfd
-       {/* <Route path="/Artistes"  element={<Artistes />}  /> */}
-        {/* <Route path="/CreateArtist" element={<CreateArtist />} />
-        <Route path="/StateArtist"  element={<StateArtist />}  />
-        <Route path="/Oeuvres" element={<Oeuvres />} />
-        <Route path="/CreateOeuvre" element={<CreateOeuvre />} />
-        <Route path="/StateOeuvre" element={<PrivateRoute isAuthenticated={isAuthenticated} element={<StateOeuvre />} />} />
-        <Route path="/Exposition" element={<PrivateRoute isAuthenticated={isAuthenticated} element={<Exposition />} />} />
-        <Route path="/CreateExposition" element={<PrivateRoute isAuthenticated={isAuthenticated} element={<CreateExposition />} />} />
-        <Route path="/StateExposition" element={<PrivateRoute isAuthenticated={isAuthenticated} element={<StateExposition />} />} />  */}
-      
+      <Sidebar/>
+      dffdfd<Link to="/Artistes">Profile</Link>
     </div>
   );
 };
